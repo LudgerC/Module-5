8a4bc486ff9675d33ae7318e376a34b0871db759
LudgerC <ludgerfilypecardoso@gmail.com>
2024-10-15 11:38:32 +0200

    Update E1.mjs

--- E1.mjs
+++ E1.mjs
@@ -2,16 +2,12 @@ import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
 const userInput = readline.createInterface({ input, output });
 
-let getal1 = parseFloat(await userInput.question("Geef getal 1 in: "));
-let getal2 = parseFloat(await userInput.question("Geef getal 2 in: "));
+let naam = await userInput.question("Wat is jou naam? ");
+
+begroeting(naam)
 
 
-som(getal1, getal2)
-console.log("Het som is: " + som)
-
-
-function som(getal1, getal2)
+function begroeting(naam)
 {
-    som = getal1 + getal2;
-    return som;
+    console.log("Goedemorgen " + naam);
 }
