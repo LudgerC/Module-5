31b6902251f6f70fd66852eaa07846ef7a78b293
LudgerC <ludgerfilypecardoso@gmail.com>
2024-10-18 14:26:28 +0200

    Create H1.mjs

---
+++ H1.mjs
@@ -0,0 +1,26 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+const userInput = readline.createInterface({ input, output });
+ 
+
+let GrootGetalReeks;
+perfectNumber(GrootGetalReeks)
+ 
+function perfectNumber(GrootGetalReeks)
+{
+    for(let i = 1 ; i < 100; i++)
+    {   let som = 0
+        for(let j = 1; j < i; j++)
+        {
+            if(i % j == 0)
+            {
+                som += j
+            }
+        }
+        if(som == i)
+        {
+            console.log(i + " is een perfect getal");
+        }    
+    }   
+
+}
